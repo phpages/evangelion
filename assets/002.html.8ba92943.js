b0dc095734b81152d8767aed5b89094e0022304d
wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-11-30 12:29:37 +0000

    Deploying to gh-pages from @ phpages/evangelion@945b3c7ae19b0ec816ab755dde1a2c4d7a8a6341 🚀

---
+++ assets/002.html.8ba92943.js
@@ -0,0 +1,3 @@
+import{_ as t}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as n,a as e,b as _}from"./app.682fd8b1.js";const s={},c=e("h1",{id:"_2022年11月30日的一件小事",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_2022年11月30日的一件小事","aria-hidden":"true"},"#"),_(" 2022年11月30日的一件小事")],-1),l=e("pre",null,[e("code",null,`                 我学会了做雪球菜单，今天异常的开心，我学会了不少指令。感觉飘飘然了， 
+`)],-1),r=e("p",null,"我不能骄傲，我应该多学点指令，把自己的脑袋爆掉。",-1),a=e("pre",null,[e("code",null,`                 哈哈，开个玩笑，我非常开心，所以有人看到这个文档后教我点指令吧！ 
+`)],-1),d=e("p",null,"对了，我的牙好疼。那就先不说啦！",-1),i=[c,l,r,a,d];function h(u,f){return o(),n("div",null,i)}const x=t(s,[["render",h],["__file","002.html.vue"]]);export{x as default};
